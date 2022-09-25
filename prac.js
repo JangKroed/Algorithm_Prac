cc8e9c16a6afe67e36007d80a7585962ca8037d2
JangKroed <didlsdydgh@gmail.com>
2022-09-26 02:16:55 +0900

    연습장

---
+++ prac.js
@@ -0,0 +1,20 @@
+// .push(element) : 배열의 뒤에 엘리먼트 추가
+// .pop() : 배열의 뒤에서 엘리먼트 삭제하고 리턴
+// .shift() : 배열의 앞에서 엘리먼트 삭제하고 리턴
+// .unshift(element) : 배열의 앞에 엘리먼트 추가
+
+
+// 문제 설명
+// for문을 이용해서 배열 cost의 값을 모두 더해 total_cost 변수에 저장하세요.
+
+// var cost = [ 85, 42, 37, 10, 22, 8, 15 ];
+// var total_cost = 0;
+// for(i = 0; i < cost.length; i++){
+//     total_cost += cost[i]
+// }
+// console.log(total_cost);
+
+
+
+
+
