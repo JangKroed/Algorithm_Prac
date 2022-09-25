a978aff7062332fbbea39a1560ff302c2b0f4955
JangKroed <didlsdydgh@gmail.com>
2022-09-26 02:16:19 +0900

    자연수 뒤집어 배열로 만들기

---
+++ Algorithm22.js
@@ -0,0 +1,44 @@
+// 자연수 뒤집어 배열로 만들기
+
+// 배열이 아닌 자연수 n을 내림차순 배열로 출력.
+// ex) n = 12345 => n = [5,4,3,2,1]
+
+// n을 문자열로 받고 (n+'')
+// 스프레드로 뿌려주고 (...)
+// 배열에 넣어주고 .reverse()
+
+function solution(n) {
+    var answer = [];
+    let a = n+''
+    for (i = 0; i < a.length; i++){
+        answer.push(+a[i])
+    }
+    return answer.reverse();
+}
+
+// ver 2
+
+function solution(...n) {
+    var answer = (n+'').split('').reverse().map(i=>Number(i))
+    return answer;
+}
+
+// 다른풀이
+function solution(n) {
+// 문자풀이
+return (n+"").split("").reverse().map(v => parseInt(v));
+
+// 숫자풀이
+var arr = [];
+
+do {
+    arr.push(n%10);
+    n = Math.floor(n/10);
+} while (n>0);
+
+return arr;
+}
+
+console.log(solution(45678))
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12932
