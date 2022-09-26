06c8cba8b29a51117f992ca2940b96f9d5c22241
JangKroed <didlsdydgh@gmail.com>
2022-09-26 23:42:41 +0900

    같은 숫자는 싫어

---
+++ Algorithm30.js
@@ -0,0 +1,15 @@
+// 같은 숫자는 싫어
+
+// 배열속 중복된 수를 제거하고 Set
+// 원배열 상태 그대로 리턴. 
+let a = [1, 1, 3, 3, 0, 1, 1]
+function solution(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] !== arr[i+1]) newArr.push(arr[i])
+    }
+    return newArr
+}
+
+console.log(solution(a))
+// https://school.programmers.co.kr/learn/courses/30/lessons/12906
