fea74395fa05258955d946a60b63fab1ec31c1de
JangKroed <didlsdydgh@gmail.com>
2022-09-26 23:33:07 +0900

    다른풀이 2 - map, sort, Math.max, Math.min

--- Algorithm29.js
+++ Algorithm29.js
@@ -64,10 +64,17 @@ console.log(solution(size));
 
 
 // 다른풀이 - reduce, Math.max, Math.min
-
 function solution(sizes) {
     const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
     return hor * ver;
+}
+
+// 다른풀이 2 - map, sort, Math.max, Math.min
+function solution(sizes) {
+    sizes.map((item) => item.sort((a, b) => b - a));
+    let hMax = Math.max.apply(null, sizes.map((item) => item[0]));
+    let vMax = Math.max.apply(null, sizes.map((item) => item[1]));
+    return hMax * vMax;
 }
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/86491
