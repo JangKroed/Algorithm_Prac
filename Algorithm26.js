090580fc0eb0cfbd9a4b7f4f0c46e042b7049c1b
JangKroed <didlsdydgh@gmail.com>
2022-09-26 17:36:53 +0900

    새로운 풀이, 어떤식으로 돌아갈지 생각해보자

--- Algorithm26.js
+++ Algorithm26.js
@@ -69,3 +69,15 @@ function collatz(num) {
   }
   return num == 1 ? answer : -1;
 }
+
+// 다른 풀이 3 - 분석해보자
+const solution = (num) => collatzGuessCount(num, 0);
+
+const collatzGuessCount = (num, acc) =>
+  num === 1
+    ? acc > 500
+      ? -1
+      : acc
+    : collatzGuessCount(processCollatz(num), acc + 1);
+
+const processCollatz = (num) => (num % 2 === 0 ? num / 2 : num * 3 + 1);
