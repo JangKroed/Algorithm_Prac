0967e1cbd61720157ff025f3bc742f7eca4ef497
JangKroed <didlsdydgh@gmail.com>
2022-09-26 17:33:31 +0900

    for문 풀이 추가

--- Algorithm26.js
+++ Algorithm26.js
@@ -45,3 +45,20 @@ function solution(num) {
     return count
 }
 
+// 다른 풀이 - for
+function collatz(num) {
+
+    for(var answer = 0;answer<500;answer++){
+      if(num%2==0){
+        num = num/2;
+      }
+      else if(num==1){
+          return answer;
+      }
+      else if(num%2==1){
+        num = (num*3)+1;
+      }
+    }
+    //if(answer == 500){return -1;}
+      return -1;
+  }
