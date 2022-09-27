5c6dc75925610e91fb2d2f0bfd948111d3220caf
JangKroed <didlsdydgh@gmail.com>
2022-09-27 16:05:05 +0900

    heap 수정

--- heap/memo.js
+++ heap/memo.js
@@ -14,23 +14,23 @@
 // 문자 크기비교는 아스키코드 기반해서 가능
 
 class MaxHeap {
-    // #은 프라이빗 해시필드 - 런타임에서 참조가 안됨. 은닉.
-  #heap = [null];
+    // 은 프라이빗 해시필드 - 런타임에서 참조가 안됨. 은닉.
+  heap = [null];
   heapify(arr) {
     for (const n of arr) {
         this.push(n);
     }
   }
   show() {
-    return this.#heap
+    return this.heap
   }
   push(n) {
-    this.#heap.push(n);
-    let child = this.#heap.length - 1;
+    this.heap.push(n);
+    let child = this.heap.length - 1;
     let parent = Math.floor(child / 2);
 
-    while (this.#heap[parent] !== null && this.#heap[parent] < this.#heap[child]) {
-      [this.#heap[parent], this.#heap[child]] = [this.#heap[child], this.#heap[parent]];
+    while (this.heap[parent] !== null && this.heap[parent] < this.heap[child]) {
+      [this.heap[parent], this.heap[child]] = [this.heap[child], this.heap[parent]];
 
       child = parent;
       parent = Math.floor(child / 2);
@@ -38,31 +38,31 @@ class MaxHeap {
   }
   // 값을 하나 꺼낼때 마다 노드를 재정렬해야함. 최적화하는 과정 필요.
   pop() {
-    const result = this.#heap[1];
+    const result = this.heap[1];
 
-    // let parent = this.#heap[2] > this.#heap[3] ? 2 : 3;
-    // this.#heap[1] = this.#heap[parent];
-    // let child = this.#heap[parent * 2] < this.#heap[parent * 2 + 1] ? parent * 2 + 1 : parent * 2;
+    // let parent = this.heap[2] > this.heap[3] ? 2 : 3;
+    // this.heap[1] = this.heap[parent];
+    // let child = this.heap[parent * 2] < this.heap[parent * 2 + 1] ? parent * 2 + 1 : parent * 2;
 
-    // while (this.#heap[parent] < this.#heap[child]{
-    //     [this.#heap[parent], this.#heap[child]] = [this.#heap[child], this.#heap[parent]];
+    // while (this.heap[parent] < this.heap[child]{
+    //     [this.heap[parent], this.heap[child]] = [this.heap[child], this.heap[parent]];
 
     //     parent = child;
-    //     child = this.#heap[parent * 2] < this.#heap[parent * 2 + 1] ? parent * 2 + 1 : parnet * 2;
+    //     child = this.heap[parent * 2] < this.heap[parent * 2 + 1] ? parent * 2 + 1 : parnet * 2;
     // }
-    // console.log(this.#heap)
+    // console.log(this.heap)
     //-------------------------------------------------------------------------------------------------
-    if (this.#heap.length === 2) return this.#heap.pop()
-    this.#heap[1] = this.#heap.pop();
+    if (this.heap.length === 2) return this.heap.pop()
+    this.heap[1] = this.heap.pop();
 
     let parent = 1;
-    let child = this.#heap[2] > this.#heap[3] ? 2 : 3;
+    let child = this.heap[2] > this.heap[3] ? 2 : 3;
 
-    while (this.#heap[parent] < this.#heap[child]) {
-        [this.#heap[parent], this.#heap[child]] = [this.#heap[child], this.#heap[parent]];
+    while (this.heap[parent] < this.heap[child]) {
+        [this.heap[parent], this.heap[child]] = [this.heap[child], this.heap[parent]];
 
         parent = child;
-        child = this.#heap[parent * 2] > this.#heap[parent * 2 + 1] ? parent * 2 : parent * 2 + 1
+        child = this.heap[parent * 2] > this.heap[parent * 2 + 1] ? parent * 2 : parent * 2 + 1
     }
 
     return result;
@@ -73,7 +73,7 @@ class MaxHeap {
 // const arr =[1,2,3,4,5,6,7,8,9,10]
 // const arr = Array.from(Array(10), (_) => Math.random().toPrecision(3))
 // const arr = [1,2,3,4,5,6,7,8,9,10]
-const arr = Array.from(Array(100000), (_) => Math.random().toPrecision(1) * 10)
+const arr = Array.from(Array(10000000), (_) => Math.random().toPrecision(3))
 
 const forHeap = arr.slice();
 const forSort = arr.slice();
