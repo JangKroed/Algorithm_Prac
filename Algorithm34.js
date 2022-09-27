c9d8833c3b7100b7017296a5c3c690672d355759
JangKroed <didlsdydgh@gmail.com>
2022-09-27 21:57:56 +0900

    문자열 내 마음대로 정렬하기

--- Algorithm34.js
+++ Algorithm34.js
@@ -1,82 +1,90 @@
-// 34번 모의고사
-// 강의 풀이
+// 문자열 내 마음대로 정렬하기
 
-function solution(answers) {
-    // 1번 수포자 '1,2,3,4,5'
-    // 2번 수포자 '2,1,2,3,2,4,2,5'
-    // 3번 수포자 '3,3,1,1,2,2,4,4,5,5'
+// 문자열로 구성된 배열 strings, 정수 n
+// 각 문자열의 인덱스 n번째를 기준으로 오름차순 정렬
+// ex)
+// strings = ["sun", "bed", "car"] 일이고 n=1일때,
+// 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬
 
-    // 문자열인지 숫자열인지 확인해보는게 좋음
-    const one = [1,2,3,4,5]
-    const two = [2,1,2,3,2,4,2,5]
-    const thr = [3,3,1,1,2,2,4,4,5,5]
+// sort로 배열속 단어 선택가능 ?
+// let strs = ["sun", "bed", "car"];
+// console.log(strs[0][1]);
 
-    const results = [0,0,0] // 첫번째 배열부터 1번수포자
+// 반복문으로 strings의 length만큼 돌아가며
+// strings의 [i][1]을 기준으로 sort
+let strings = ["sun", "bed", "car"];
+let n = 1;
+// function solution(strings, n) {
+//   let a = strings;
+//   for (let j = 0; j <= n; j++) {
+//     for (let i = 0; i < a.length; i++) {
+//       if (a[i]?.charAt(n) > a[i + 1]?.charAt(n)) {
+//         [a[i], a[i + 1]] = [a[i + 1], a[i]];
+//       }
+//     }
+//   }
+//   return a;
+// }
+// 이 방법은 문자가 같을때 그다음 문자 정렬불가..
 
-    for(let i = 0; i < answers.length; i++){
-        // 수포자들의 찍는 방식이 달라 각각 다르게 설정
-        if(one[i%one.length] === answers[i]) results[0]++;
-        if(two[i%one.length] === answers[i]) results[1]++;
-        if(thr[i%one.length] === answers[i]) results[2]++;
+// function solution(strings, n) {
+//   let arr = [];
+//   for (let i = 0; i < strings.length; i++) {
+//     arr.push("" + strings[i].charAt(n) + strings[i]);
+//   }
+//   arr.sort();
+//   let answer = [arr.length];
+//   for (let i = 0; i < arr.length; i++) {
+//     answer.push(arr.shift(i).substring(1, arr.shift(i).length));
+//   }
+//   return answer;
+// } // 자바보고 해볼라햇는데 어렵네..;
+
+function solution(strings, n) {
+  let answer = strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1;
+    if (a[n] < b[n]) return -1;
+    if (a[n] === b[n]) {
+      if (a > b) return 1;
+      if (a < b) return -1;
+      return 0;
     }
-    // ...은 스프레드 연산자 (배열을 벗겨준다)
+  });
 
-    const maxNumber = Math.max(...result)
-    // results = [4,5,5]
-    // maxNumber = 5;
-    let answer = [];
-    // i = 0; answer = [];
-    // i = 1; answer = [2];
-    // i = 2; answer = [2, 3];
+  return answer;
+}
 
-    for(let i = 0; i < results.length; i++){
-        if(maxNumber === results[i]) answer.push(i + 1) // 인덱스는 0부터 이고 첫번째 학생 인덱스값은 0이므로 +1
+// 다른 풀이 - sort, localeCompare
+function solution(strings, n) {
+  // strings 배열
+  // n 번째 문자열 비교
+  return strings.sort((a, b) =>
+    a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n])
+  );
+}
+
+console.log(solution(strings, n));
+
+// 다른 풀이 2 - 자바보고 해보고싶던거, 
+function solution(strings, n) {
+    var answer = [];
+    for (var i = 0; i < strings.length; i++) {
+        var chu = strings[i][n];
+        strings[i] = chu + strings[i];
     }
+    strings.sort();
+    for (var j = 0; j < strings.length; j++) {
+        strings[j] = strings[j].replace(strings[j][0],"");
+        answer.push(strings[j])
+    }
+
     return answer;
 }
 
-// 현업 코딩테스트 기준 크레인 인형뽑기 수준이다.
+// https://school.programmers.co.kr/learn/courses/30/lessons/12915
 
-// 강의 풀의 주석판
-// 34번 모의고사
+// let a = 'a'
+// let b = 'b'
+// let c = 'c'
 
-function solution(answers) {
-    //1,2,3번 수포자들의 찍는 패턴을 배열 변수로 할당시킵니다.
-    const one = [1,2,3,4,5];
-    const two = [2,1,2,3,2,4,2,5];
-    const three = [3,3,1,1,2,2,4,4,5,5];
-    
-    //results는 각 수포자들이 몇개의 문제를 맞췄는지 각 배열에 요소에 기입합니다.
-    //results[0] = 1번 수포자가 맞춘개수, results[1] = 2번 수포자가 맞춘개수, results[2] = 3번 수포자가 맞춘개수.
-    const results = [0,0,0];
-    
-    //answers에 들어가 있는 각 문제의 답들을 수포자들이 맞췄는지 알기위해서
-    //answers의 길이만큼 반복문을 돌립니다.
-    for(let i = 0; i < answers.length; i++){
-        
-        //첫번째 수포자가 각 문제를 맞추면 results[0]에 1씩 더합니다.
-        if(one[i%5] === answers[i]) results[0]++;
-        
-        //두번째 수포자가 각 문제를 맞추면 results[1]에 1씩 더합니다.
-        if(two[i%8] === answers[i]) results[1]++;
-        
-        //세번째 수포자가 각 문제를 맞추면 results[2]에 1씩 더합니다.
-        if(three[i%10] === answers[i]) results[2]++;
-    }
-    
-    //1,2,3 수포자가 맞춘 개수중에 제일 많이 맞춘 개수를 구합니다.
-    const maxNum = Math.max(...results);
-    const answer = [];
-    
-    //1,2,3 수포자중에 제일 많이 맞춘 수포자(들)을 찾아서 answer 배열에 내림차순으로 집어넣습니다.
-    for(let i = 0; i < results.length; i++){
-        
-        //각 수포자들이 맞춘 문제의 개수와 제일 많이 맞춘 문제의 개수를 비교해서 같으면 answer 배열에 집어넣습니다.
-        //i+1이 각 수포자를 뜻합니다.
-        if(results[i] === maxNum) answer.push(i+1);
-    }
-    
-    
-    
-    return answer;
-}
+// console.log(a.localeCompare(c))
