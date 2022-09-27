0a270952ca46090b70b7ad248fcae5613e5cf950
JangKroed <didlsdydgh@gmail.com>
2022-09-28 04:26:31 +0900

    신규아이디 추천 안되서 정규식없는 다른 풀이 추가

--- Algorithm39.js
+++ Algorithm39.js
@@ -62,4 +62,21 @@ function is_Digit(num) {
 
 console.log(solution(new_id));
 
+// 안돼서 정규식없는 다른 풀이
+function solution(nid) {
+  var ans = "";
+  for (let i = 0; i < nid.length; i++) {
+    let c = nid[i].toLowerCase();
+    if ("0123456789abcdefghijklmnopqrstuvwxyz.-_".indexOf(c) === -1) continue;
+    if (c === "." && ans[ans.length - 1] === "." && nid[i - 1]) continue;
+    ans += c;
+  }
+  if (ans[0] === ".") ans = ans.slice(1);
+  ans = ans.slice(0, 15);
+  if (ans[ans.length - 1] === ".") ans = ans.slice(0, ans.length - 1);
+  if (!ans) ans = "a";
+  while (ans.length < 3) ans += ans[ans.length - 1];
+  return ans;
+}
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/72410
