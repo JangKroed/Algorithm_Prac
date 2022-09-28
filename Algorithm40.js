cc11018e9a247786f844a327d343d4ee1616c9c9
JangKroed <didlsdydgh@gmail.com>
2022-09-28 12:40:10 +0900

    제곱근이 정수면 약수의 개수가 홀수다...

--- Algorithm40.js
+++ Algorithm40.js
@@ -53,6 +53,7 @@ console.log(solution(left, right));
 function solution(left, right) {
   var answer = 0;
   for (let i = left; i <= right; i++) {
+    // 제곱근이 정수면 약수의 개수가 홀수다...
     if (Number.isInteger(Math.sqrt(i))) {
       answer -= i;
     } else {
