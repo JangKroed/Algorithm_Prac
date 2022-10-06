31b79a3b3b8d934d6cb5224458cdb88f825ab6bd
JangKroed <didlsdydgh@gmail.com>
2022-10-06 23:03:42 +0900

    두 수의 차

---
+++ beginner/Algorithm01.js
@@ -0,0 +1,12 @@
+// 두 수의 차
+
+/**
+ * 정수 num1과 num2가 주어질때
+ * num1에서 num2를 뺀 값을 return
+ */
+
+function solution(num1, num2) {
+  return num1 - num2;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
