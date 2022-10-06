8cad245941a52ef8f3fd68390fb9faab386748e7
JangKroed <didlsdydgh@gmail.com>
2022-10-06 23:05:51 +0900

    문자열 뒤집기

---
+++ beginner/Algorithm14.js
@@ -0,0 +1,11 @@
+// 문자열 뒤집기
+
+/**
+ * 문자열 my_string을 뒤집은 문자열을 리턴
+ */
+
+function solution(my_string) {
+  return my_string.split("").reverse().join("");
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120822
