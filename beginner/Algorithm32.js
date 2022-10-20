1da427ab0b77973e66eafc46ba822c8f5b2bd8e0
JangKroed <didlsdydgh@gmail.com>
2022-10-20 19:28:29 +0900

    피자 나눠 먹기 (1)

---
+++ beginner/Algorithm32.js
@@ -0,0 +1,13 @@
+// 피자 나눠 먹기 (1)
+
+/**
+ * 피자 한판이 7조각 일경우
+ * 사람수 n이 한조각씩 먹어야하는데
+ * 이때 필요한 피자의 판 수를 return
+ */
+
+function solution(n) {
+  return Math.ceil(n / 7);
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/120814
